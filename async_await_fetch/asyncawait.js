94c07906db2fcbbbea397fb0b9ef9c244f930586
anup thakur <anksthakur7@gmail.com>
2024-05-01 09:17:28 +0530

    event async await

---
+++ async_await_fetch/asyncawait.js
@@ -0,0 +1,23 @@
+// javaScript sync hothi hai
+// phle ek code run hota hai frr doosra code
+// single thread hai
+// blocking code (sync)= block the flow of program
+//means code one by one execute hota hai
+
+// non-blocking code (async) = does not block the flow of programm
+// means side by side kaam krta rhta hai
+
+// javaScript engine => JS engine work krta hai 2 ways m
+// 1 memory heap = memory allocation hota hai isme
+//2 call stack = jab bhi code execute hota hai uhska call ctack bnta hai
+
+async function getAllUser (){
+    try {
+        const response = await fetch("https://dummyjson.com/carts");
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error)
+    }
+}
+getAllUser();
--- closures/closure.js
+++ closures/closure.js
@@ -1,10 +1,22 @@
 // function makeFunc(){
 //     const name = "Anup";
 //     function inner(){
-//         console.log(name)
+//         console.log("inner",name)
 //     }
 //     return inner; // return humne pura function he kar deya 
 // }
 // const myFunc = makeFunc()
 // myFunc();
 // output Anup 
+
+
+// function outer(){
+//     let a = 20;
+//     function inner(){
+//         console.log(a) 
+//     }
+//     return inner;
+// }
+// const b = outer();
+// b();
+// 20
